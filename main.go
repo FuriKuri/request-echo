03a5bc80bb02709960323856a2902003ecf4a578
Theo Pack <tf.pack@gmail.com>
2018-01-24 18:20:19 +0100

    Add headers

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +11,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
 	request = append(request, url)
 	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
 	fmt.Fprintf(w, "%s", request)
 }
 
