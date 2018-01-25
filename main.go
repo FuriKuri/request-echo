0b619748e8c4509a4e23ae3aa2b1556104450724
Theo Pack <tf.pack@gmail.com>
2018-01-25 19:30:35 +0100

    Concat strings

--- main.go
+++ main.go
@@ -8,19 +8,19 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	var request []string
+	var request string
 	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	request += url
+	request += fmt.Sprintf("Host: %v", r.Host)
 	for name, headers := range r.Header {
 		name = strings.ToLower(name)
 		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
+			request += fmt.Sprintf("%v: %v", name, h)
 		}
 	}
 
 	bodyBuffer, _ := ioutil.ReadAll(r.Body)
-	request = append(request, fmt.Sprintf("Body: %v", string(bodyBuffer)))
+	request += fmt.Sprintf("Body: %v", string(bodyBuffer))
 
 	fmt.Fprintf(w, "%s", request)
 }
