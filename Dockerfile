fc2355f53f5266905b0c30f13f063655e27c4a6b
Theo Pack <tf.pack@gmail.com>
2019-08-16 10:31:54 +0200

    Update echo

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM golang:1.9
+FROM golang:1.12
 ADD . /go/src/github.com/furikuri/request-echo
 WORKDIR /go/src/github.com/furikuri/request-echo
 RUN go get ./
 RUN go build
 
-FROM alpine:3.6
+FROM alpine:3.10
 RUN mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
 WORKDIR /root/
 COPY --from=0 /go/bin/request-echo .
--- main.go
+++ main.go
@@ -4,23 +4,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	var request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	url := fmt.Sprintf("%v %v %v\n", r.Method, r.URL, r.Proto)
 	request += url
-	request += fmt.Sprintf("Host: %v", r.Host)
+	request += fmt.Sprintf("Host: %v\n", r.Host)
 	for name, headers := range r.Header {
-		name = strings.ToLower(name)
+		// name = strings.ToLower(name)
 		for _, h := range headers {
-			request += fmt.Sprintf("%v: %v", name, h)
+			request += fmt.Sprintf("%v: %v\n", name, h)
 		}
 	}
 
 	bodyBuffer, _ := ioutil.ReadAll(r.Body)
-	request += fmt.Sprintf("Body: %v", string(bodyBuffer))
+	request += fmt.Sprintf("Body: %v\n", string(bodyBuffer))
 
 	fmt.Fprintf(w, "%s", request)
 }
